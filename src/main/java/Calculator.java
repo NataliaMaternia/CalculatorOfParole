e2c0f1948971695330230eaa6d9657356bc5789f
Natalia Maternia <nat.maternia@gmail.com>
2024-02-15 15:10:09 +0100

    loading input data

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,12 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Scanner;
 
 public class Calculator {
 
     LocalDate firstDayInPrison;
-
+    Period imprisonment;
+    String summary;
     public Calculator() {
     }
 
@@ -19,9 +21,32 @@ public class Calculator {
             throw new IllegalArgumentException("Data rozpoczęcia kary pozbawienia wolności" +
                     "nie może być późniejsza niż dziesiejsza data");
         }
-        Period timeInPrison = Period.between(localDate(), firstDayInPrison);
+        Period timeInPrison = Period.between(firstDayInPrison, localDate());
         return timeInPrison;
     }
+
+    public void imprisonment(Scanner scanner) {
+        System.out.println("Wpisz lata");
+        int yearOfImprisonment = scanner.nextInt();
+        System.out.println("Wpisz miesiące");
+        int monthsOfImprisonment = scanner.nextInt();
+        final int dayOfImprisonment = 0;
+        imprisonment = Period.of(yearOfImprisonment,monthsOfImprisonment,dayOfImprisonment);
+        summary = yearOfImprisonment + " lat " + monthsOfImprisonment +
+                " miesięcy pozbawienia wolności";
+    }
+
+    public void firstDayInPrison(Scanner scanner) {
+        System.out.println("Wpisz rok");
+        int imprisonmentStartYear = scanner.nextInt();
+        System.out.println("Wpisz miesiac");
+        int imprisonmentStarMonth = scanner.nextInt();
+        System.out.println("Wpisz dzień");
+        int imprisonmentStartDay = scanner.nextInt();
+        firstDayInPrison = LocalDate.of(imprisonmentStartYear, imprisonmentStarMonth, imprisonmentStartDay);
+    }
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,42 @@
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Calculator calculator = new Calculator();
         System.out.println(calculator.localDate());
-        System.out.println(calculator.timeInPrison());
+
+        Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("Program oblicza datę warunkowego przedterminowego zwolnienia. " +
                 "Wpisz karę orzeczoną przez Sąd. Przykład: Jeżeli Sąd orzekł karę " +
                 "2 lat i 6 miesięcy pozbawienia wolności. " +
-                "W polu \"lata\" wpisz 2, w polu \"mięsięcy\" wpisz 6 ");
+                "W polu \"lata\" wpisz 2, w polu \"mięsięsiące\" wpisz 6 ");
 
+        calculator.imprisonment(scanner);
 
         System.out.println("Wpisz datę rozpoczęcia odbywania kary pozbawienia wolności. " +
                 "Przykład: gdy datą rozpoczęcia kary jest 23 luty 2022r., w pole \"rok\" wpisz 2022, " +
                 "w pole \"miesiąc\" wpisz 2, a w pole \"dzień\" wpisz 23");
-        Scanner parole = new Scanner(System.in);
-        System.out.println("Wpisz rok");
-        int year = parole.nextInt();
-        System.out.println("Wpisz miesiac");
-        int month = parole.nextInt();
-        System.out.println("Wpisz dzień");
-        int day = parole.nextInt();
-        calculator.firstDayInPrison = LocalDate.of(year, month,day );
-        parole.close();
-
-        System.out.println(calculator.firstDayInPrison);
-
+        calculator.firstDayInPrison(scanner);
+        System.out.println("Sąd orzekł karę: " + calculator.summary);
+        System.out.println("W dniu " + calculator.firstDayInPrison +
+                " rozpoczęto wykonywanie kary pozbawienia wolności");
+        System.out.println("Do tej pory wykonano " + formatPeriodToString(calculator.timeInPrison())
+                + " kary pozbawienia wolności");
+        System.out.println();
+        scanner.close();
     }
+
+    public static String formatPeriodToString(Period period) {
+        String format = period.getYears() + " lat " + period.getMonths()
+                + " miesięcy " + period.getDays() + " dni";
+        return format;
+    }
+
+
 }
