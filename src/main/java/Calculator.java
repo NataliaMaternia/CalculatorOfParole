8943cbf46b8187b34fb75c966c565de14ad6a1db
Natalia Maternia <nat.maternia@gmail.com>
2024-02-20 14:36:58 +0100

    calculate period of parole ifLifetimeImprisonment

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,6 +11,8 @@ public class Calculator {
     int yearOfImprisonment;
     int monthsOfImprisonment;
     int dayOfImprisonment;
+    boolean lifeImprisonment;
+    boolean ifLifeImprisonment;
 
     public Calculator() {
     }
@@ -30,15 +32,28 @@ public class Calculator {
         return timeInPrison;
     }
 
+    public boolean lifeImprisonment(Scanner scanner) {
+        System.out.println("Czy została orzeczona kara dożywotniego pozbawienia wolności? " +
+                "Wpisz 'tak' lub 'nie'");
+        ifLifeImprisonment = scanner.next().contains("tak");
+        return ifLifeImprisonment;
+    }
+
+
     public void imprisonment(Scanner scanner) {
-        System.out.println("Wpisz lata");
-        yearOfImprisonment = scanner.nextInt();
-        System.out.println("Wpisz miesiące");
-        monthsOfImprisonment = scanner.nextInt();
-        dayOfImprisonment = 0;
-        imprisonment = Period.of(yearOfImprisonment, monthsOfImprisonment, dayOfImprisonment);
-        summary = yearOfImprisonment + " lat " + monthsOfImprisonment +
-                " miesięcy pozbawienia wolności";
+        if (!ifLifeImprisonment) {
+            System.out.println("Wpisz karę orzeczoną przez Sąd. Przykład: Jeżeli Sąd orzekł karę" +
+                    " 2 lat i 6 miesięcy pozbawienia wolności. " +
+                    "W polu \"lata\" wpisz 2, w polu \"mięsiące\" wpisz 6");
+            System.out.println("Wpisz lata");
+            yearOfImprisonment = scanner.nextInt();
+            System.out.println("Wpisz miesiące");
+            monthsOfImprisonment = scanner.nextInt();
+            dayOfImprisonment = 0;
+            imprisonment = Period.of(yearOfImprisonment, monthsOfImprisonment, dayOfImprisonment);
+            summary = yearOfImprisonment + " lat " + monthsOfImprisonment +
+                    " miesięcy pozbawienia wolności";
+        }
     }
 
     public void firstDayInPrison(Scanner scanner) {
@@ -51,21 +66,21 @@ public class Calculator {
         firstDayInPrison = LocalDate.of(imprisonmentStartYear, imprisonmentStarMonth, imprisonmentStartDay);
     }
 
-    public Period calculateParole() {
-        if (yearOfImprisonment < 25) {
+    public LocalDate calculateParole() {
+        if (ifLifeImprisonment) {
+            LocalDate lifeImprisonmentTrue = firstDayInPrison.plusYears(25);
+            return lifeImprisonmentTrue;
+        } else if (yearOfImprisonment < 25) {
             int yearsOfImprisonmentInMonths = yearOfImprisonment * 12;
             int totalMonths = yearsOfImprisonmentInMonths + monthsOfImprisonment;
             int calculatedMonthsOfParole = totalMonths / 2;
             int calculatedDaysOfParole = totalMonths % 2;
             Period periodOfParole = Period.of(0, calculatedMonthsOfParole, calculatedDaysOfParole);
-            return periodOfParole.normalized();
-//            Period paroleAfterHalfImprisonment = Period.of(yearOfImprisonment,
-//                    monthsOfImprisonment,dayOfImprisonment);
-//            paroleAfterHalfImprisonment.getYears() / 2;
-        } else {
-            return null;
+            return firstDayInPrison.plus(periodOfParole.normalized());
+        } else if (yearOfImprisonment >= 25) {
+            LocalDate imprisonmentMoreThan25years = firstDayInPrison.plusYears(15);
+            return imprisonmentMoreThan25years;
         }
+        return null;
     }
-
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,21 +9,16 @@ public class Main {
         Calculator calculator = new Calculator();
         System.out.println(calculator.localDate());
 
+        System.out.println("Program oblicza datę warunkowego przedterminowego zwolnienia. ");
+
         Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Program oblicza datę warunkowego przedterminowego zwolnienia. " +
-                "Wpisz karę orzeczoną przez Sąd. Przykład: Jeżeli Sąd orzekł karę " +
-                "2 lat i 6 miesięcy pozbawienia wolności. " +
-                "W polu \"lata\" wpisz 2, w polu \"mięsięsiące\" wpisz 6 ");
-
+        calculator.lifeImprisonment(scanner);
         calculator.imprisonment(scanner);
-
         System.out.println("Wpisz datę rozpoczęcia odbywania kary pozbawienia wolności. " +
                 "Przykład: gdy datą rozpoczęcia kary jest 23 luty 2022r., w pole \"rok\" wpisz 2022, " +
                 "w pole \"miesiąc\" wpisz 2, a w pole \"dzień\" wpisz 23");
         calculator.firstDayInPrison(scanner);
-        System.out.println("Sąd orzekł karę: " + calculator.summary);
+        System.out.println("Sąd orzekł karę: " + calculator.imprisonment);
         System.out.println("W dniu " + calculator.firstDayInPrison +
                 " rozpoczęto wykonywanie kary pozbawienia wolności");
         System.out.println("Do tej pory wykonano " + formatPeriodToString(calculator.timeInPrison())
@@ -31,9 +26,16 @@ public class Main {
         System.out.println("Zgodnie z art. 78. § 1. Skazanego można warunkowo zwolnić po odbyciu co najmniej\n" +
                 "połowy kary, a jeżeli wymierzono karę pozbawienia wolności na czas nie krótszy niż\n" +
                 "25 lat – po odbyciu co najmniej 15 lat kary.");
+        System.out.println("Skazany może ubiegać się o przedterminowe warunkowe zwolnienie z dniem "
+                + calculator.calculateParole());
+
+        System.out.println("Pamiętaj, że zastosowanie przez Sąd warunkowego przdterminowego zwolnienia nie jest " +
+                "obligatoryjne. Sąd sąd może zwolnić z odbycia reszty kary, gdy spełnione zostają " +
+                "przesłanki określone w art. xxxx");
 
 // data od kiedy siedzi plus period.normalized
       //System.out.println("Sąd może zastosować warunkowe przedterminowe zwolnienie z dniem ");
+
 
         scanner.close();
     }
